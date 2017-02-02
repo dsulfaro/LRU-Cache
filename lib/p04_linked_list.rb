bdab68417721f4045013fe0fe8a9e29e9da0f0b2
Dan Sulfaro <dsulfaro@nickdonn-mbp.local>
2017-02-01 22:40:09 -0800

    aA's HashMap and LinkedList

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+# Credit - App Academy
 class Link
   attr_accessor :key, :val, :next, :prev
 
@@ -19,15 +20,17 @@ class Link
     self.prev = nil
     self
   end
+
 end
 
 class LinkedList
   include Enumerable
 
   def initialize
-    @head = nil
-    @tail = nil
-    @size = 0
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -36,84 +39,65 @@ class LinkedList
   end
 
   def first
-    @head
+    empty? ? nil : @head.next
   end
 
   def last
-    @tail
+    empty? ? nil : @tail.prev
   end
 
   def empty?
-    @head == nil && @tail == nil
+    @head.next == @tail
   end
 
   def get(key)
-    ptr = @head
-    while ptr != nil
-      return ptr.val if ptr.key == key
-      ptr = ptr.next
-    end
+    each { |link| return link.val if link.key == key }
     nil
   end
 
   def include?(key)
-    ptr = @head
-    qtr = @tail
-    while ptr != qtr
-      return true if ptr.key == key
-      return true if qtr.key == key
-      ptr = ptr.next
-      qtr = qtr.prev
-    end
-    false
+    any? { |link| link.key == key }
   end
 
   def append(key, val)
     new_link = Link.new(key, val)
-    if @head == nil && @tail == nil
-      @head = new_link
-      @tail = new_link
-    else
-      @tail.next = new_link
-      @tail.next.prev = @tail
-      @tail = @tail.next
-    end
+
+    @tail.prev.next = new_link
+    new_link.prev = @tail.prev
+    new_link.next = @tail
+    @tail.prev = new_link
+
+    new_link
   end
 
   def update(key, val)
-    each do |l|
-      puts l.to_s
-      if l.key == key
-        l.val = val
-        return l
+    each do |link|
+      if link.key == key
+        link.val = val
+        return link;
       end
     end
-    nil
   end
 
   def remove(key)
-    if @head != nil && @head.key == key
-      @head = @head.next
-      return
-    end
-    each do |l|
-      if l.key == key
-        l.remove
-        return l.val
+    each do |link|
+      if link.key == key
+        link.remove
+        return link.val
       end
     end
+
     nil
   end
 
   def each
-    curr = @head
-    until curr == nil
-      yield curr
-      curr = curr.next
+    current_link = @head.next
+    until current_link == @tail
+      yield current_link
+      current_link = current_link.next
     end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
   def to_s
     inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
   end
