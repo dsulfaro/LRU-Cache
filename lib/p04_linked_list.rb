d8c176adb2df86dd58a199f068d88c1260b5c98b
Dan Sulfaro <dsulfaro@nickdonn-mbp.local>
2017-02-01 20:39:37 -0800

    finished doubly linked list

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -13,13 +13,21 @@ class Link
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    self.prev.next = self.next if self.prev
+    self.next.prev = self.prev if self.next
+    self.next = nil
+    self.prev = nil
+    self
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = nil
+    @tail = nil
+    @size = 0
   end
 
   def [](i)
@@ -28,34 +36,85 @@ class LinkedList
   end
 
   def first
+    @head
   end
 
   def last
+    @tail
   end
 
   def empty?
+    @head == nil && @tail == nil
   end
 
   def get(key)
+    ptr = @head
+    while ptr != nil
+      return ptr.val if ptr.key == key
+      ptr = ptr.next
+    end
+    nil
   end
 
   def include?(key)
+    ptr = @head
+    qtr = @tail
+    while ptr != qtr
+      return true if ptr.key == key
+      return true if qtr.key == key
+      ptr = ptr.next
+      qtr = qtr.prev
+    end
+    false
   end
 
   def append(key, val)
+    new_link = Link.new(key, val)
+    if @head == nil && @tail == nil
+      @head = new_link
+      @tail = new_link
+    else
+      @tail.next = new_link
+      @tail.next.prev = @tail
+      @tail = @tail.next
+    end
   end
 
   def update(key, val)
+    each do |l|
+      puts l.to_s
+      if l.key == key
+        l.val = val
+        return l
+      end
+    end
+    nil
   end
 
   def remove(key)
+    if @head != nil && @head.key == key
+      @head = @head.next
+      return
+    end
+    each do |l|
+      if l.key == key
+        l.remove
+        return l.val
+      end
+    end
+    nil
   end
 
   def each
+    curr = @head
+    until curr == nil
+      yield curr
+      curr = curr.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
